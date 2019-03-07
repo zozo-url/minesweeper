1294fd36ca079b6f0963f5bbe61ed8b6772bfb4f
zozo-url <zozo-url@protonmail.ch>
2019-03-07 18:59:30 +1300

    Create board

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,62 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [{
+    row: 0,
+    col: 0,
+    isMine: false,
+    hidden: true
+  },
+  {
+    row: 0,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 0,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 1,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 1,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 1,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 2,
+    col: 0,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 2,
+    col: 1,
+    isMine: false,
+    hidden: true,
+  },
+  {
+    row: 2,
+    col: 2,
+    isMine: false,
+    hidden: true,
+  }]
+}
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
